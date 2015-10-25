b8ea305665a478e1873d71191348b0c7e593d7f5
coasterboy101 <coasterboy101@gmail.com>
2015-10-25 16:48:10 -0600

    Encapsulated the database functions in a class, should help with both organization and reliability.

---
+++ Timesheet/DatabaseFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Timesheet
+{
+	class DatabaseFunctions
+	{
+		private MySqlConnection dbConnection;
+
+		/// <summary>
+		/// Runs a stored procedure and returns a datatable containing the result set.
+		/// </summary>
+		/// <param name="connString">The database connection string.</param>
+		/// <param name="procName">The name of the stored procedure to execute.</param>
+		/// <param name="parameters">The parameters to supply to stored procedure with.</param>
+		/// <returns></returns>
+		public DataTable FillStoredProc(string connString, string procName, List<MySqlParameter> parameters)
+		{
+			using (dbConnection = new MySqlConnection(connString))
+			{
+				if (dbConnection.State != ConnectionState.Open) dbConnection.Open();
+
+				MySqlCommand dbCommand = new MySqlCommand(procName, dbConnection);
+				dbCommand.CommandType = CommandType.StoredProcedure;
+
+				// Loop through the parameters, and add each one to dbCommand's parameter collection.
+				foreach (MySqlParameter param in parameters)
+				{
+					dbCommand.Parameters.Add(param);
+				}
+
+				// Fill the datatable with the result set, then return it.
+				DataTable dbTable = new DataTable("");
+				MySqlDataAdapter dbAdapter = new MySqlDataAdapter(dbCommand);
+				dbAdapter.Fill(dbTable);
+
+				return dbTable;
+			}
+		}
+	}
+}
--- Timesheet/frmLogin.cs
+++ Timesheet/frmLogin.cs
@@ -12,41 +12,47 @@ namespace Timesheet
 {
 	public partial class frmLogin : Form
 	{
-		private DataTable dtEmployees;
+		private DatabaseFunctions dbFunctions;
+
+		private DataTable dtUsers;
 
 		public frmLogin()
 		{
 			InitializeComponent();
 
-			dtEmployees = new DataTable("Employees");
+			dbFunctions = new DatabaseFunctions();
+
+			dtUsers = new DataTable("Employees");
 		}
 
 		private void btnSubmit_Click(object sender, EventArgs e)
 		{
-			using (MySqlConnection dbConnection = new MySqlConnection(Properties.Settings.Default.timesheetConnectionString))
+			List<MySqlParameter> cmdParams = new List<MySqlParameter>();
+			cmdParams.Add(new MySqlParameter("p_EmpId", Convert.ToInt32(txtUserId.Text)));
+			cmdParams.Add(new MySqlParameter("p_Password", txtPassword.Text));
+
+			dtUsers = dbFunctions.FillStoredProc(Properties.Settings.Default.timesheetConnectionString, "Auth_User", cmdParams);
+
+			// Check the given user ID and password against the database, if it is incorrect, there will be no rows.
+			if (dtUsers.Rows.Count != 0)
 			{
-				dbConnection.Open();
-
-				MySqlCommand dbCommand = new MySqlCommand("LoadUsers", dbConnection);
-				dbCommand.CommandType = CommandType.StoredProcedure;
-				dbCommand.Parameters.Add(new MySqlParameter("p_EmpId", txtUserId.Text));
-				dbCommand.Parameters.Add(new MySqlParameter("p_Password", txtPassword.Text));
-
-				MySqlDataAdapter dbAdapter = new MySqlDataAdapter(dbCommand);
-				dbAdapter.Fill(dtEmployees);
-
-				dbConnection.Close();
-			}
-
-			if (dtEmployees.Rows.Count != 0)
-			{
-				frmTimesheet newForm = new frmTimesheet(dtEmployees.Rows[0].ItemArray[2].ToString());
-				newForm.Show();
-				this.Hide();
+				// Make sure that they are still employed.
+				if (Convert.ToInt32(dtUsers.Rows[0]["Status"].ToString()) == 1)
+				{
+					frmTimesheet newTimesheet = new frmTimesheet(dtUsers.Rows[0]["EmployeeType"].ToString());
+					newTimesheet.Show();
+					this.Hide();
+				}
+				else
+				{
+					MessageBox.Show("You are no longer listed as an employee. Please contact your administrator if you beleive this"
+							+ " is an error.", "Authentication Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
 			}
 			else
 			{
-				MessageBox.Show("The User ID or Password you have entered is incorrect.", "Authentication Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+				MessageBox.Show("The User ID or Password you have entered is incorrect.", "Authentication Error", 
+						MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 	}
